a2217fe3e176c84197916532c3ff7ad53ccf0922
KagazezhevB <boris.kagazezhev@ru.axxonsoft.com>
2021-12-05 23:34:48 +0300

    'v2'

--- bot.py
+++ bot.py
@@ -11,12 +11,15 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.reply("Привет!")
+    await message.reply("Hello user! \n Enter your city: ")
 
 
 @dp.message_handler()
 async def request_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, get_weather(msg.text))
+    try:
+        await bot.send_message(msg.from_user.id, get_weather(msg.text))
+    except KeyError:
+        await bot.send_message(msg.from_user.id, "Undefined city, try again: ")
 
 
 if __name__ == '__main__':
--- openweather_api.py
+++ openweather_api.py
@@ -3,10 +3,10 @@ from configure import OPEN_WEATHER_KEY
 
 
 def get_weather(city):
-    req = requests.get(
-        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_KEY}").json()
-    weather_desc, grade = req["weather"][0]["description"], req["main"]["temp"] - 273.15
-    return weather_desc, format(grade, '.2f')
+        req = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_KEY}").json()
+        weather_desc, grade = req["weather"][0]["description"], req["main"]["temp"] - 273.15
+        return f"Weather description - {weather_desc} \n Air temperature - {format(grade, '.2f')}" + u'\u2103'
 
 
-print(get_weather("Санкт-Петербург"))
+
