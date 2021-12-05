248920f5cf0e8abf0d10511b5420f7bbf920755a
KagazezhevB <91414564+KagazezhevB@users.noreply.github.com>
2021-12-06 00:33:53 +0300

    Update openweather_api.py

--- openweather_api.py
+++ openweather_api.py
@@ -7,6 +7,3 @@ def get_weather(city):
             f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_KEY}").json()
         weather_desc, grade = req["weather"][0]["description"], req["main"]["temp"] - 273.15
         return f"Weather description - {weather_desc} \n Air temperature - {format(grade, '.2f')}" + u'\u2103'
-
-
-
